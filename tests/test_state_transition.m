99c87ab2aee2df1839d7fcd4de25159af60ecdfd
Bill Fan <1472451850@qq.com>
2021-03-17 02:15:26 -0400

    Tests: Created basic testing framework. Need to add testing later

---
+++ tests/test_state_transition.m
@@ -0,0 +1,17 @@
+function tests = test_state_transition
+tests = functiontests(localfunctions);
+end
+
+%% Test if body states are updated correctly in the case of a purely linear motion
+% TODO: Description of the test scenario
+function testPureLinMvmtBodyStates(testCase)
+
+end
+
+%% Test if body states are updated correctly in the case of a purely rotational motion
+% TODO: Description of the test scenario
+function testPureRotMvmtBodyStates(testCase)
+
+end
+
+%% Test for pure
