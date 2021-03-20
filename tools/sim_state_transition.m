f4e3dad9aa239de8f668af06e591298b100d8df9
Bill Fan <1472451850@qq.com>
2021-03-20 18:07:09 -0400

    Tools: Cleaned up state transition sim struct arr impl

--- tools/sim_state_transition.m
+++ tools/sim_state_transition.m
@@ -1,12 +1,22 @@
-function [t, states] = sim_state_transition(states_0, inputs, dt)
+function [t, structs_states, mat_states] = sim_state_transition(v_states_0, mat_inputs, dt)
 
-states = zeros(size(states_0, 1), size(inputs, 2) + 1);
+mat_states = zeros(size(v_states_0, 1), size(mat_inputs, 2) + 1);
 
-states(:, 1) = states_0;
-t = zeros(1, size(inputs, 2) + 1);
+mat_states(:, 1) = v_states_0;
+struct_states_0 = state_vec_to_struct(v_states_0);
+structs_states = repelem(struct_states_0, 1, size(mat_inputs, 2) + 1);
 
-for i = 1 : size(inputs, 2)
-    states(:, i + 1) = predict_state_transition(states(:, i), inputs(:, i), 0, dt);
+t = zeros(1, size(mat_inputs, 2) + 1);
+
+for i = 1 : size(mat_inputs, 2)
+    
+    v_state_next = predict_state_transition(mat_states(:, i), mat_inputs(:, i), 0, dt);
+    
+    mat_states(:, i+1) = v_state_next;
+    
+    % Not sure if there's a better way to preallocate than this
+    structs_states(1, i+1) = state_vec_to_struct(v_state_next);
+    
     t(i+1) = i*dt;
 end
 
