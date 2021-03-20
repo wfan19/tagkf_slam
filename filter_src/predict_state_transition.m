7b5bf981a451f9f12d0de7a887d635d4b047958c
Bill Fan <1472451850@qq.com>
2021-03-20 13:28:43 -0400

    Filter: State transition now uses state structs

--- filter_src/predict_state_transition.m
+++ filter_src/predict_state_transition.m
@@ -1,4 +1,4 @@
-function states_next = predict_state_transition(states, u, ~, dt)
+function v_states_next = predict_state_transition(v_states, v_inputs, ~, dt)
 % Function input:
 %   states_now: states x 1 column vector of current states
 %   u: 6 x 1 column vector of IMU measurements
@@ -6,10 +6,10 @@ function states_next = predict_state_transition(states, u, ~, dt)
 % This is an implementation of the quaternion based EKF predict functions
 % found in the following two papers:
 % For a general overview, and the world-frame position equation:
-% https://arxiv.org/pdf/1606.05285.pdf
+% https://arxiv.org/pdf/1606.05285.pdf (Paper 1)
 %
 % For specifically integrating Apriltags and camera extrinsics, see here:
-% https://arxiv.org/pdf/1507.02081.pdf
+% https://arxiv.org/pdf/1507.02081.pdf (Paper 2)
 
 % Vector conventions:
 %{
@@ -38,55 +38,66 @@ Where:
 
 %}
 
-%% Fetch state and input vectors
-% TODO: figure out a better way to do this (wrap in a function call?)
-% States:
-posn_body = states(1:3);
-vel_body = states(4:6);
-quat_body = quaternion(states(7:10)');
-
-% bias_f = states(11:13);
-% bias_omega = states(14:16);
-
-bias_f = 0;
-bias_omega = 0;
-
-% Inputs:
-f_meas = u(1:3);
-omega_meas = u(4:6);
+%% Convert input vectors to structs for sanity
+states = state_vec_to_struct(v_states);
+inputs = input_vec_to_struct(v_inputs);
+states_next = state_vec_to_struct(zeros(30, 1));
 
 %% Preprocess data
 % Correct for bias and noise in measurements
-f_corrected = f_meas - bias_f; % TODO: Subtract w term
-omega_corrected = omega_meas - bias_omega; % TODO: Subtract w term
+f_corrected = inputs.f - states.bias_f; % TODO: Subtract w term
+omega_corrected = inputs.omega - states.bias_omega; % TODO: Subtract w term
 
 % Create skew symmetric matrix of omega
 % - mat_omega * v is equivalent to cross(omega, v)
 % - mat_omega is an element of the lie algebra of SO(3)
-% See https://arxiv.org/pdf/1812.01537.pdf for more on skew symmetric
+% See https://arxiv.org/pdf/1812.01537.pdf for more on skew symmetricvel_camera = rotatepoint(quat_vb, 
 % matrices and how they're a lie algebra of SO(3)
 mat_omega = mat_skew_sym(omega_corrected);
 
 %% Calculate state transitions
 % Predict next position
-posn_body_next = posn_body + transpose(dt*(rotatepoint(quat_body, vel_body(:)'))); % TODO: Add w term
+states_next.posn_body = states.posn_body + ...
+                    transpose(dt*(rotatepoint(states.quat_body, states.vel_body(:)'))); % TODO: Add w term
 
 % Predict next velocity
+g_rotated = transpose(rotatepoint(states.quat_body, [0, 0, 0])); % Turns off gravity
 %g_rotated = transpose(rotatepoint(quat_body, [0, 0, -9.8])); % Gravity acceleration vector
-g_rotated = transpose(rotatepoint(quat_body, [0, 0, 0])); % Turns off gravity
-vel_body_next = vel_body + ...
-                dt*(g_rotated + f_corrected - mat_omega*vel_body);
+states_next.vel_body = states.vel_body + ...
+                dt*(g_rotated + f_corrected - mat_omega * states.vel_body);
 
 % Predict next orientation
-% The 
-quat_body_next = quat_body * quaternion(rotm2quat(expm(dt*mat_omega)));
-v_quat_body_next = compact(quat_body_next);
+% - See Equation (56) from Paper (1)
+% - expm is the matrix exponential / exponential map
+% - The exponential map of an angular velocity matrix (the lie algebra) is
+%   the corresponding change in 3d rotation (the lie group)
+states_next.quat_body = states.quat_body * quaternion(rotm2quat(expm(dt*mat_omega)));
 
-%% Set output state vector
-states_next = zeros(size(states));
-states_next(1:3) = posn_body_next(:);
-states_next(4:6) = vel_body_next(:);
-states_next(7:10) = v_quat_body_next(:);
+% "Predict" next biases and camera extrinsics
+% In this case, it's just adding a noise term
+% TODO: Actually add the noise term instead of feeding forward current state
+states_next.bias_f = states.bias_f;
+states_next.bias_omega = states.bias_omega;
+
+states_next.posn_bv = states.posn_bv;
+states_next.quat_vb = states.quat_vb;
+
+% Predict next tag position in the camera frame
+% Equation (13) from simplifies to this if you distribute out matrix and
+% quaternion multiplications
+
+states_next.posn_tag = states.posn_tag;
+%mat_omega_camera = rotatepoint(states.quat_vb, omega_corrected(:)'); % Body angular vel in the camera frame
+%states_next.pos_tag = states.posn_tag + ...
+                        %dt*(-mat_omega_camera * states.posn_tag - ...
+                        %rotatepoint(states.quat_vb, omega_corrected * states.posn_bv) + states.vel_body);
+
+% Predict next tag orientation in the camera frame
+states_next.quat_tag = quaternion([1, 0, 0, 0]);
+
+%% Generate output state vector
+
+v_states_next = state_struct_to_vec(states_next);
 
 end
 
