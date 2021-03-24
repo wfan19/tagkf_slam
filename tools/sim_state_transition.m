d95e1f970ecf1acf767ea3de92fa4dbf5867fed3
Bill Fan <1472451850@qq.com>
2021-03-24 12:45:52 -0400

    Tools: State transition simulation now lets you choose if you want to use ode45

--- tools/sim_state_transition.m
+++ tools/sim_state_transition.m
@@ -1,5 +1,10 @@
-function [t, structs_states, mat_states] = sim_state_transition(v_states_0, mat_inputs, dt)
+function [t, structs_states, mat_states] = sim_state_transition(v_states_0, mat_inputs, dt, use_ode45)
 
+% Check if optional argument exists
+if ~exist("use_ode45", "var")
+    use_ode45 = false;
+end
+    
 mat_states = zeros(size(v_states_0, 1), size(mat_inputs, 2) + 1);
 
 mat_states(:, 1) = v_states_0;
@@ -8,9 +13,16 @@ structs_states = repelem(struct_states_0, 1, size(mat_inputs, 2) + 1);
 
 t = zeros(1, size(mat_inputs, 2) + 1);
 
+% Generate prediction function handle
+if use_ode45
+    f_predict = @(states, inputs, noise, dt)(predict_state_transition_ode45(states, inputs, noise, dt));
+else
+    f_predict = @(states, inputs, noise, dt)(predict_state_transition(states, inputs, noise, dt));
+end
+
 for i = 1 : size(mat_inputs, 2)
     
-    v_state_next = predict_state_transition(mat_states(:, i), mat_inputs(:, i), 0, dt);
+    v_state_next = f_predict(mat_states(:, i), mat_inputs(:, i), 0, dt);
     
     mat_states(:, i+1) = v_state_next;
     
