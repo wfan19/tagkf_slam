ffc764c2212de312531bfb5bd4f4ded43b567097
Bill Fan <1472451850@qq.com>
2021-04-04 03:04:52 -0400

    Tools: Tag body-world frame conversion script now also converts orientations

--- tools/tags_body_to_world.m
+++ tools/tags_body_to_world.m
@@ -2,14 +2,20 @@ function tab_tag_states = tags_body_to_world(tab_states)
 % Converts body frame tag positions and orientations to world frame
 % positions and orientations
 
-
 tab_tag_states = table();
 
+%% Calculate world-frame positions
 posn_bv_rotated_world_orntn = rotatepoint(tab_states.quat_body, tab_states.posn_bv);
 posn_tag_rotated_world_orntn = rotatepoint(tab_states.quat_body, rotatepoint(conj(tab_states.quat_vb), tab_states.posn_tag));
 
+%% Calculate world-frame orientations
+quat_tag = tab_states.quat_tag .* tab_states.quat_vb .* tab_states.quat_body;
+
+%% Assign output table states
 tab_tag_states.posn_tag = tab_states.posn_body + ...
                 posn_bv_rotated_world_orntn + posn_tag_rotated_world_orntn;
+            
+tab_tag_states.quat_tag = quat_tag;
 
 end
 
