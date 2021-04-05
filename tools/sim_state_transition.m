30744a1f3f77d81926dd7eecdb9ce5b49f38e87c
Bill Fan <1472451850@qq.com>
2021-04-05 02:55:07 -0400

    Tools: Updated state transition sim to use state structs

--- tools/sim_state_transition.m
+++ tools/sim_state_transition.m
@@ -1,35 +1,25 @@
-function [t, structs_states, mat_states] = sim_state_transition(v_states_0, mat_inputs, dt, use_ode45)
+function [t, structs_states] = sim_state_transition(states_0, mat_inputs, dt, use_ode45)
 
 % Check if optional argument exists
 if ~exist("use_ode45", "var")
     use_ode45 = false;
 end
-    
-mat_states = zeros(size(v_states_0, 1), size(mat_inputs, 2) + 1);
 
-mat_states(:, 1) = v_states_0;
-struct_states_0 = state_vec_to_struct(v_states_0);
-structs_states = repelem(struct_states_0, 1, size(mat_inputs, 2) + 1);
+structs_states = repelem(states_0, 1, size(mat_inputs, 2) + 1);
 
 t = zeros(1, size(mat_inputs, 2) + 1);
 
 % Generate prediction function handle
 if use_ode45
-    f_predict = @(states, inputs, noise, dt)(predict_state_transition_ode45(states, inputs, noise, dt));
+    f_predict = @(state, input, noise, dt)(predict_state_transition_ode45(state, input, noise, dt));
 else
-    f_predict = @(states, inputs, noise, dt)(predict_state_transition(states, inputs, noise, dt));
+    f_predict = @(state, input, noise, dt)(predict_state_transition(state, input, noise, dt));
 end
 
+% Run the simulation
 for i = 1 : size(mat_inputs, 2)
-    
-    v_state_next = f_predict(mat_states(:, i), 0, mat_inputs(:, i), dt);
-    
-    mat_states(:, i+1) = v_state_next;
-    
-    % Not sure if there's a better way to preallocate than this
-    structs_states(1, i+1) = state_vec_to_struct(v_state_next);
-    
     t(i+1) = i*dt;
+    structs_states(1, i+1) = f_predict(structs_states(1, i), 0, mat_inputs(:, i), dt);
 end
 
 end
